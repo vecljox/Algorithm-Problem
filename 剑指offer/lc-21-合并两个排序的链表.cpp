b40894a49d734b72e2feca847eab13b0c70d3fe0
vecljox <90954592+vecljox@users.noreply.github.com>
2024-09-24 16:31:28 +0800

    Update lc-21-合并两个排序的链表.cpp

--- 剑指offer/lc-21-合并两个排序的链表.cpp
+++ 剑指offer/lc-21-合并两个排序的链表.cpp
@@ -34,3 +34,30 @@ public:
         return dummy.next;
     }
 };
+
+
+class SolutionB {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==nullptr)
+            return list2;
+
+        if(list2==nullptr)
+            return list1;
+
+        if(list1->val<list2->val)
+        {
+            ListNode* temp = mergeTwoLists(list1->next,list2);
+            list1->next=temp;
+            
+            return list1;
+        }
+        else
+        {
+            ListNode* temp = mergeTwoLists(list1,list2->next);
+            list2->next=temp;
+
+            return list2;
+        }
+    }
+};
