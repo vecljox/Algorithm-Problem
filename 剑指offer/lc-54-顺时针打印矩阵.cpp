0b8d50f0f789aaa23e57813cc548b6f69fd86c72
vecljox <90954592+vecljox@users.noreply.github.com>
2024-09-26 13:31:55 +0800

    Create lc-54-顺时针打印矩阵.cpp

---
+++ 剑指offer/lc-54-顺时针打印矩阵.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    vector<int> res{};
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+
+        int u=0;
+        int d=matrix.size()-1;
+        int l=0;
+        int r=matrix[0].size()-1;
+
+        for(;;)
+        {
+            for(int i=l;i<=r;++i)
+                ans.push_back(matrix[u][i]);
+
+            if(++u>d)   
+                break;
+
+            for(int i=u;i<=d;++i)
+                ans.push_back(matrix[i][r]);
+
+            if(--r<l)
+                break;
+
+            for(int i=r;i>=l;--i)
+                ans.push_back(matrix[d][i]);
+
+            if(--d<u)   
+                break;
+
+            for(int i=d;i>=u;--i)
+                ans.push_back(matrix[i][l]);
+
+            if(++l>r)
+                break;
+        }
+
+        return ans;
+    }
+
+};
