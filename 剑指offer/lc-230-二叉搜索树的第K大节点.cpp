a63815d00326a782d548e3161f3434517bd72b09
vecljox <90954592+vecljox@users.noreply.github.com>
2024-10-12 17:37:59 +0800

    Create lc-230-二叉搜索树的第K大节点.cpp

---
+++ 剑指offer/lc-230-二叉搜索树的第K大节点.cpp
@@ -0,0 +1,32 @@
+// 用中序遍历迭代版本即可，若要找第K大的元素就把左根右换成右根左
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> stk;
+        while (root || !stk.empty()) {
+            while (root) {
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            if (--k == 0)
+                break;
+            root = root->right;
+        }
+        return root->val;
+    }
+};
