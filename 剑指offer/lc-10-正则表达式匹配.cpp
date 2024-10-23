d5aacdaa42f102298d1967dae547d5b2f5a7d98b
vecljox <90954592+vecljox@users.noreply.github.com>
2024-10-23 14:26:26 +0800

    Update lc-10-正则表达式匹配.cpp

--- 剑指offer/lc-10-正则表达式匹配.cpp
+++ 剑指offer/lc-10-正则表达式匹配.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int m  = s.size();
+        int m = s.size();
         int n = p.size();
 
-        auto matches= [&](int i,int j)
-        {
-            if(i==0)
+        auto matches = [&](int i, int j) {
+            if (i == 0)
                 return false;
 
-            return p[j-1]=='.'||p[j-1]==s[i-1];
+            return p[j - 1] == '.' || p[j - 1] == s[i - 1];
         };
 
-        vector<vector<char>> dp = vector<vector<char>>(m+1,vector<char>(n+1,false));
+        vector<vector<char>> dp(m + 1, vector<char>(n + 1, false));
 
-        dp[0][0]=true;
+        dp[0][0] = true;
 
-        for(int i =0;i<=m;++i)
-        {
-            for(int j=1;j<=n;++j)
-            {
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
                 // if(j+1<=n&&p[j]=='*')
                 //     continue;
-                    
-                if(p[j-1]!='*')
-                {
-                    if(matches(i,j))
-                    {
-                        dp[i][j]=dp[i-1][j-1];
-                    }
-                }
-                else
-                {
-                    dp[i][j]=dp[i][j-2];
 
-                    if(matches(i,j-1))
-                        dp[i][j]|=dp[i-1][j];
+                if (p[j - 1] != '*') {
+                    if (matches(i, j)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                } else {
+                    dp[i][j] = dp[i][j - 2];
+
+                    if (matches(i, j - 1))
+                        dp[i][j] |= dp[i - 1][j];
                 }
             }
         }
