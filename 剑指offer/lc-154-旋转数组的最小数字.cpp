1023e384b1dac3fc817de618aa10f64b345b54c6
vecljox <90954592+vecljox@users.noreply.github.com>
2024-10-31 14:32:05 +0800

    Update lc-154-旋转数组的最小数字.cpp

--- 剑指offer/lc-154-旋转数组的最小数字.cpp
+++ 剑指offer/lc-154-旋转数组的最小数字.cpp
@@ -1,26 +1,38 @@
+// https://www.acwing.com/solution/content/6386/
+
+// 直接二分
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int left = 0 ;
-        int right = nums.size()-1;
-        int center;
-
-        while((left<right)&&(nums[left]>=nums[right]))
-        {
-            center = (left+right)/2;
-
-            if(nums[center]<nums[left])
-                right = center;
-
-            else if(nums[center]>nums[right])
-                left = center+1;
-                
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] < nums[r])
+                r = mid;
+            else if (nums[mid] > nums[r])
+                l = mid + 1;
             else
-                ++left;
+                --r;
         }
-
-        return nums[left];
-
+        return nums[l];
     }
-    
+};
+
+// 先去重再二分
+class SolutionB {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while (r > 0 && nums[0] == nums[r])
+            --r;
+        int check = nums[r];
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] <= check)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return nums[l];
+    }
 };
