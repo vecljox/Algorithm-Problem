5e82d54b22faa56fb7e3b870083d2170bea27d34
vecljox <90954592+vecljox@users.noreply.github.com>
2024-11-02 00:17:29 +0800

    Create lc-42-接雨水.cpp

---
+++ LeetCode Hot 100/lc-42-接雨水.cpp
@@ -0,0 +1,71 @@
+// 双指针
+// 注意循环条件不能写l<=r，否则可能会导致越界
+// 不用考虑l=r的原因是此时柱子为最高柱子(left和right中总是有一个停留的位置是当前已遍历过列中最高的柱子)，没有雨水
+// 如果想写成l<=r则要把更新lmax和rmax的代码放在l++,r--前面，不过不推荐这样做，因为反而没现在简洁
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int res = 0, n = height.size();
+        int l = 0, r = n - 1, lmax = height[l], rmax = height[r];
+        while (l < r) {
+            if (lmax < rmax) {
+                res += lmax - height[l++];
+                lmax = max(lmax, height[l]);
+            } 
+            else {
+                res += rmax - height[r--];
+                rmax = max(rmax, height[r]);
+            }
+        }
+        return res;
+    }
+};
+
+// (三次线性扫描) 
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+class SolutionB {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> lmax(n), rmax(n);
+        lmax[0] = height[0];
+        for (int i = 1; i < n; ++i) {
+            lmax[i] = max(lmax[i - 1], height[i]);
+        }
+        rmax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            rmax[i] = max(rmax[i + 1], height[i]);
+        }
+        int res = 0;
+        for (int i = 1; i < n - 1; ++i) {
+            res += min(lmax[i], rmax[i]) - height[i];
+        }
+        return res;
+    }
+};
+
+// 单调栈,个人感觉挺简洁但思路有点难
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+class SolutionC {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size(), res = 0;
+        stack<int> stk;
+        for (int i = 0; i < n; ++i) {
+            while (!stk.empty() && height[i] > height[stk.top()]) {
+                int top = stk.top();
+                stk.pop();
+                if (stk.empty())
+                    break;
+                res += (i - stk.top() - 1) *
+                       (min(height[i], height[stk.top()]) - height[top]);
+            }
+            stk.push(i);
+        }
+        return res;
+    }
+};
