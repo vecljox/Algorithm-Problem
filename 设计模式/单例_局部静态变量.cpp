9495df62ff5c2d050487045ee07151c94bee1a7f
vecljox <90954592+vecljox@users.noreply.github.com>
2024-11-03 13:29:02 +0800

    Update and rename 单例.cpp to 单例_局部静态变量.cpp

--- 设计模式/单例_局部静态变量.cpp
+++ 设计模式/单例_局部静态变量.cpp
@@ -5,11 +5,8 @@ class Singleton
 public:
 	static Singleton* getInstance()
 	{
-		if (m_pInstance == nullptr)
-		{
-			m_pInstance = new Singleton();
-		}
-		return m_pInstance;
+		static Singleton instance;
+		return &instance;
 	}
 
 	Singleton(const Singleton&) = delete;
@@ -20,7 +17,6 @@ public:
 	}
 
 private:
-	static Singleton* m_pInstance;
 	std::string m_string;
 	Singleton() {
 		m_string = "test";
@@ -31,11 +27,9 @@ private:
 	}
 };
 
-Singleton* Singleton::m_pInstance = nullptr;
-
 int main()
 {
 	Singleton::getInstance()->doSomeOperation();
+	Singleton::getInstance()->doSomeOperation();
 	return 0;
 }
-
