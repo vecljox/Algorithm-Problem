07028ec82f6f3e51bfe2901af2776d6c1f31c34e
vecljox <90954592+vecljox@users.noreply.github.com>
2024-11-06 11:50:54 +0800

    Update lc-64-最小路径和.cpp

--- LeetCode Hot 100/lc-64-最小路径和.cpp
+++ LeetCode Hot 100/lc-64-最小路径和.cpp
@@ -28,11 +28,10 @@ public:
         int m = grid.size(), n = grid[0].size();
         vector<int> dp(n);
         dp[0] = grid[0][0];
-        for (int i = 1; i < n; ++i) {
-            dp[i] = dp[i - 1] + grid[0][i];
-        }
+        for (int j = 1; j < n; ++j)
+            dp[j] = dp[j - 1] + grid[0][j];
         for (int i = 1; i < m; ++i) {
-            dp[0] += grid[i][0];  //单独计算第0列
+            dp[0] += grid[i][0]; // 单独计算第0列
             for (int j = 1; j < n; ++j)
                 dp[j] = min(dp[j], dp[j - 1]) + grid[i][j];
         }
