6a54300489aa84938b7eb00aba1ba2209c80c960
vecljox <90954592+vecljox@users.noreply.github.com>
2024-12-04 22:05:05 +0800

    Update lc-84-柱状图中最大的矩形.cpp

--- LeetCode Hot 100/lc-84-柱状图中最大的矩形.cpp
+++ LeetCode Hot 100/lc-84-柱状图中最大的矩形.cpp
@@ -8,16 +8,14 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         int res = 0;
         int n = heights.size();
-        heights.push_back(0);
         stack<int> stk;
+        heights.push_back(0);
         for (int i = 0; i <= n; i++) {
             while (!stk.empty() && heights[i] < heights[stk.top()]) {
                 int cur = stk.top();
                 stk.pop();
-                if (stk.empty())
-                    res = max(res, heights[cur] * i);
-                else
-                    res = max(res, heights[cur] * (i - stk.top() - 1));
+                int width = stk.empty() ? i : i - stk.top() - 1;
+                res = max(res, heights[cur] * width);
             }
             stk.push(i);
         }
