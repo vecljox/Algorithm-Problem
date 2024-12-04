740d452455f7fc44071acc6ce2b6fb03d5b2fb52
vecljox <90954592+vecljox@users.noreply.github.com>
2024-12-04 19:11:56 +0800

    Update lc-79-矩阵中的路径.cpp

--- 剑指offer/lc-79-矩阵中的路径.cpp
+++ 剑指offer/lc-79-矩阵中的路径.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
+    int m, n;
     bool exist(vector<vector<char>>& board, string word) {
-
-        int row = board.size();
-        int col  = board[0].size();
-        int len = word.length();
-
-        for(int i =0;i<row;++i)
-        {
-            for(int j =0;j<col;++j)
-            {
-                if(dfs(board,i,j,word,0))
+        m = board.size(), n = board[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, i, j, word, 0))
                     return true;
             }
         }
-
         return false;
     }
 
-    bool dfs(vector<vector<char>>& board,int i, int j, string word,int len)
-    {
-        int row = board.size();
-        int col  = board[0].size();
-
-        if(i<0||i>=row||j<0||j>=col||board[i][j]!=word[len])
+    bool dfs(vector<vector<char>>& board, int i, int j, const string& word,
+             int len) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[len])
             return false;
 
-        if(len == word.length()-1)
+        if (len == word.size() - 1)
             return true;
 
-        board[i][j]='\v';
+        board[i][j] = '\0';    //标记被访问过
 
-        bool res = dfs(board,i,j+1,word,len+1)
-            ||dfs(board,i+1,j,word,len+1)
-            ||dfs(board,i,j-1,word,len+1)
-            ||dfs(board,i-1,j,word,len+1);
+        bool res = dfs(board, i, j + 1, word, len + 1) ||
+                   dfs(board, i + 1, j, word, len + 1) ||
+                   dfs(board, i, j - 1, word, len + 1) ||
+                   dfs(board, i - 1, j, word, len + 1);
 
-        board[i][j]=word[len];
+        board[i][j] = word[len];
 
         return res;
     }
