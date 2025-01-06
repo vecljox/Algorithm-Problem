98e65fad12b4d843af5008c095468ae22465167f
vecljox <90954592+vecljox@users.noreply.github.com>
2025-01-06 23:05:34 +0800

    Update lc-84-柱状图中最大的矩形.cpp

--- LeetCode Hot 100/lc-84-柱状图中最大的矩形.cpp
+++ LeetCode Hot 100/lc-84-柱状图中最大的矩形.cpp
@@ -3,14 +3,17 @@
 // 为了算法书写方便，在数组末尾添加高度 0
 // 这会使得栈中所有数字在最后出栈。
 
+
+//如果找到的是当前柱形条左侧第一个和自己相等的柱形条，虽然算出来的面积不是真正符合题意的矩形面积，但是当计算到相等的柱形条时会覆盖现在的情况，所以对于题目要求没有影响
+
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int res = 0;
+        heights.push_back(-1);
         int n = heights.size();
         stack<int> stk;
-        heights.push_back(0);
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; ++i) {
             while (!stk.empty() && heights[i] < heights[stk.top()]) {
                 int cur = stk.top();
                 stk.pop();
