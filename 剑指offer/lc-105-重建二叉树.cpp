026ad2557ce5dbc0fe069d3cbd44482a48902e4e
vecljox <90954592+vecljox@users.noreply.github.com>
2025-01-30 16:58:14 +0800

    Update lc-105-重建二叉树.cpp

--- 剑指offer/lc-105-重建二叉树.cpp
+++ 剑指offer/lc-105-重建二叉树.cpp
@@ -1,39 +1,32 @@
 // 用哈希表存储根节点元素在inorder中的索引
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
+
 class Solution {
-    unordered_map<int, int> pos;
+    unordered_map<int, int> index;
 
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = preorder.size();
-        for (int i = 0; i < n; ++i) {
-            pos[inorder[i]] = i;
-        }
-        return dfs(preorder, inorder, 0, n - 1, 0, n - 1);
+        for (int i = 0; i < n; ++i)
+            index[inorder[i]] = i;
+        return buildTree(preorder, inorder, 0, n - 1, 0, n - 1);
     }
 
-    TreeNode* dfs(vector<int>& preorder, vector<int>& inorder, int pl, int pr,
-                  int il, int ir) {
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder, int pl,
+                        int pr, int il, int ir) {
         if (pl > pr)
             return nullptr;
+
         int val = preorder[pl];
-        int k = pos[val];
-        int len = k - il;
+        int rootIndexInInorder = index[val];
         auto root = new TreeNode(val);
-        root->left = dfs(preorder, inorder, pl + 1, pl + len, il, k - 1);
-        root->right = dfs(preorder, inorder, pl + len + 1, pr, k + 1, ir);
+
+        int left_pr = pl + (rootIndexInInorder - il);
+        root->left = buildTree(preorder, inorder, pl + 1, left_pr, il,
+                               rootIndexInInorder - 1);
+        root->right = buildTree(preorder, inorder, left_pr + 1, pr,
+                                rootIndexInInorder + 1, ir);
+
         return root;
     }
 };
