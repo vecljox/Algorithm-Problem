bc916af2db52fb092c7260ab75c85a87be08b269
vecljox <90954592+vecljox@users.noreply.github.com>
2025-02-02 16:27:28 +0800

    Update mergeSort.cpp

--- 算法实现/mergeSort.cpp
+++ 算法实现/mergeSort.cpp
@@ -1,3 +1,4 @@
+// 递归版本，自顶向下
 void mergeSort(vector<int> &nums, int l, int r)
 {
     if (l >= r)
@@ -23,3 +24,38 @@ void mergeSort(vector<int> &nums, int l, int r)
     for (int i = l, j = 0; i <= r; ++i, ++j)
         nums[i] = tmp[j];
 }
+
+// 迭代版本，自底向上
+std::vector<int> mergeSort(std::vector<int> &nums)
+{
+    int n = nums.size();
+    std::vector<int> tmp(n);
+    for (int i = 1; i < n; i *= 2)
+    {
+        int k = 0;
+        for (int j = 0; j < n; j += 2 * i)
+        {
+            int p = j, q = j + i;
+            int end_p = std::min(j + i, n), end_q = std::min(j + 2 * i, n);
+
+            while (p < end_p && q < end_q)
+            {
+                if (nums[p] <= nums[q])
+                    tmp[k++] = nums[p++];
+                else
+                    tmp[k++] = nums[q++];
+            }
+            while (p < end_p)
+            {
+                tmp[k++] = nums[p++];
+            }
+            while (q < end_q)
+            {
+                tmp[k++] = nums[q++];
+            }
+        }
+        nums.swap(tmp);
+    }
+    return nums;
+}
+
