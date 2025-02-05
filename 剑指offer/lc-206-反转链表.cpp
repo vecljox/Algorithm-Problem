6259db3f95231f0885adc640019b6a91fbfcf94e
vecljox <90954592+vecljox@users.noreply.github.com>
2025-02-05 19:34:09 +0800

    Update lc-206-反转链表.cpp

--- 剑指offer/lc-206-反转链表.cpp
+++ 剑指offer/lc-206-反转链表.cpp
@@ -1,30 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        // if(head==nullptr||head->next==nullptr)
-        //     return head;
-
-        ListNode* pre = nullptr;
-        ListNode* cur = head;
-        while(cur!=nullptr)
-        {
-            ListNode* tmp =cur->next;
-            cur->next = pre;
-            pre=cur;
-            cur=tmp;
+        ListNode dummy;
+        dummy.next = nullptr;
+        while (head) {
+            auto tmp = head->next;
+            head->next = dummy.next;
+            dummy.next = head;
+            head = tmp;
         }
 
-        return pre;
+        return dummy.next;
     }
 };
 
