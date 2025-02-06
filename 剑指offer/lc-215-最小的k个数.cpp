82d1a27a19c1622bf3998b649f031c542edb01b6
vecljox <90954592+vecljox@users.noreply.github.com>
2025-02-07 01:50:49 +0800

    Update lc-215-最小的k个数.cpp

--- 剑指offer/lc-215-最小的k个数.cpp
+++ 剑指offer/lc-215-最小的k个数.cpp
@@ -2,36 +2,37 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int len = nums.size();
-        //建堆
-        for (int i = len / 2 - 1; i >= 0; --i) {
-            percDown(nums, i, len);
+        int n = nums.size();
+        // 构建最大堆
+        for (int i = (n - 1) / 2; i >= 0; --i) {
+            percDown(nums, i, n);
         }
-        //弹出k-1个最大元素
-        for (int j = len - 1; --k > 0; --j) {
-            nums[0] = nums[j];
-            percDown(nums, 0, j);
+        // 执行 k-1 次删除堆顶操作
+        for (int i = n - 1; --k > 0; --i) {
+            nums[0] = nums[i];
+            percDown(nums, 0, i);
         }
         return nums[0];
     }
 
-    //下滤  i:下滤的起点索引值 n:堆的逻辑大小 
     void percDown(vector<int>& nums, int i, int n) {
-        int tmp = nums[i];
-        int child;
-        for (; leftChild(i) < n; i = child) {
-            child = leftChild(i);
-            if (child != n - 1 && nums[child] < nums[child + 1])
+        int tmp = nums[i]; // 保存当前节点的值
+        while (2 * i + 1 < n) {
+            int child = 2 * i + 1; // 左子节点
+            // 如果右子节点存在且比左子节点大，则选择右子节点
+            if (child != n - 1 && nums[child] < nums[child + 1]) {
                 ++child;
-            if (tmp < nums[child])
+            }
+            // 如果子节点比当前节点大，则交换
+            if (tmp < nums[child]) {
                 nums[i] = nums[child];
-            else
+                i = child;
+            } else {
                 break;
+            }
         }
-        nums[i] = tmp;
+        nums[i] = tmp; // 将 tmp 放到最终的位置
     }
-
-    inline int leftChild(int i) { return 2 * i + 1; }
 };
 
 //快速排序的分区方法
